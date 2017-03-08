f519de8b930cf4c9bf23c61431434150da31d7b3
Marcus Barcelos <marcus.barcelos@bluesoft.com.br>
2017-03-08 11:27:35 -0300

    Configuring Docker
    Script to create an image and run using docker.

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+tag=$(git symbolic-ref HEAD 2>/dev/null | awk -F '/' '{print $NF}')
+mvn clean package docker:build -DdockerImageTags=$tag
+docker run -p :8080 --rm -ti desafio-bluesoft:$tag
