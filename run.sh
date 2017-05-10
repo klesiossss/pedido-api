79ff2b45b875d21181b2cb46c382deb220314e90
Marcus Barcelos <marcus.barcelos@bluesoft.com.br>
2017-05-10 17:21:07 -0300

    running docker in backgroud

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 tag=$(git symbolic-ref HEAD 2>/dev/null | awk -F '/' '{print $NF}')
 mvn clean package docker:build -DdockerImageTags=$tag
-docker run -p :8080 --rm -ti desafio-bluesoft:$tag
+docker run -p :8080 -d desafio-bluesoft:$tag
